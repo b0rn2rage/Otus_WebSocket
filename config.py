08c6dc90d1ca5f11d2b72f7f71ca6a97e23c5f10
Anton <b0rn2rage@bk,ru>
2021-05-22 19:11:59 +0300

    create socket

---
+++ config.py
@@ -0,0 +1,7 @@
+import random
+
+LOCALHOST = "127.0.0.1"
+
+
+def random_port():
+    return random.randint(10000, 30000)
---
+++ http_socket.py
@@ -0,0 +1,26 @@
+import http_socket
+from config import LOCALHOST, random_port
+
+BACKLOG = 10
+
+my_socket = http_socket.http_socket()
+address_and_port = (LOCALHOST, random_port())
+my_socket.bind(address_and_port)
+
+print("Socket bind on =", address_and_port)
+
+my_socket.listen(BACKLOG)
+
+conn, address = my_socket.accept()
+
+# При подключении возвращает параметры соединения и адрес клиента
+print("connection:", conn)
+print("address:", address)
+
+# Получаем данные из соединения
+data = conn.recv(1024)
+
+conn.send(
+    f"HTTP/1.1 200 OK\r\n Content-Length: 100\r\n Connection: close\r\n Content-Type: text/html\r\n\n "
+    f"<h3>{data.decode()}</h3>".encode("utf-8"))
+my_socket.close()
